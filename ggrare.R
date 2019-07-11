64f5d7ceb20b560b81d54808b3ec66d79d99b719
Mark N. Read <>
2019-07-11 11:19:03 +1000

    typo

--- ggrare.R
+++ ggrare.R
@@ -9,7 +9,7 @@
 #' @export
 
 ggrare <- function(physeq_object, step = 10, label = NULL, color = NULL, plot = TRUE, parallel = FALSE, se = TRUE,
-                   col_per_grup = NULL, line_weight=0.2) 
+                   col_per_group = NULL, line_weight=0.2) 
 {
   x <- methods::as(phyloseq::otu_table(physeq_object), "matrix")
   if (phyloseq::taxa_are_rows(physeq_object)) { x <- t(x) }
@@ -68,8 +68,8 @@ ggrare <- function(physeq_object, step = 10, label = NULL, color = NULL, plot = 
                                            y = ".S",
                                            group = "Sample",
                                            color = color))
-  if (! is.null(col_per_grup))
-    p <- p + scale_colour_manual(values=col_per_grup)
+  if (! is.null(col_per_group))
+    p <- p + scale_colour_manual(values=col_per_group)
   
   p <- p + ggplot2::labs(x = "Number of sequences", y = "Unique ASVs found per sample")
   
