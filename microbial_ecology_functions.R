16cf970715c8aae6b4b58a7a07d46d950e32552e
Mark N. Read <>
2019-12-09 22:07:37 +1100

    Group differences in aitchison space - graphs optional, p and KS values saved to filesystem.

--- microbial_ecology_functions.R
+++ microbial_ecology_functions.R
@@ -108,6 +108,8 @@ within_between_group_distances = function(
   # Euclidean distance between CLR-transformed data recommended, as it copes well with compositional data. 
   distance_matrix,  # n x n matrix of distances between items. 
   group_vector,  # Eg column from metadata file. Order of items must match those in distance matrix.
+  datasave_prefix=NA,
+  graph_distributions=TRUE,  # Be cautious, can create a lot of plots - all pairwise of groups. 
   plotsave_prefix='',  # To save to a different directory, provide path through this prefix.
   # If TRUE, plot separate distributions for within groups A and B distances. Otherwise, combine them.
   separate_within_distributions=FALSE,
@@ -116,7 +118,7 @@ within_between_group_distances = function(
   plot_size = c(34, 22)  # mm
 )
 {
-  dir.create(plotsave_prefix, showWarnings = FALSE)  # Creat if it doesn't already exist. 
+  dir.create(plotsave_prefix, showWarnings = FALSE)  # Create if it doesn't already exist. 
   if(is.factor(group_vector) == FALSE)
   {  stop("Please supply the group_vector as a factor.")  }
   
@@ -156,59 +158,65 @@ within_between_group_distances = function(
     # Adjust for multiple comparisons. 
     p_value = p.adjust(ks$p.value, method='bonferroni', n=num_comparisons)
     
-    if (separate_within_distributions == FALSE) {
-      # ggplot needs data in dataframe format
-      df = data.frame(Distance=c(within_group_distances, between_group_distances), 
-                      Condition=c(rep('Within', length(within_group_distances)),
-                                  rep('Between', length(between_group_distances))))
-      p = ggplot(df, aes(x=Distance, fill=Condition)) + 
-          # There are other options; this shows subtble differences in distribution.  
-          # http://www.cookbook-r.com/Graphs/Plotting_distributions_(ggplot2)/
-          geom_density(alpha=.3) +
-          theme(plot.background = element_rect(fill = "transparent", color = NA)) + # Transparent plot background. 
-          coord_cartesian(xlim = xlimits)
-    } else {
-      # ggplot needs data in dataframe format
-      df = data.frame(Distance=c(within_group_distances, between_group_distances), 
-                      Condition=c(rep(paste('Within ', group_a, sep=''), length(a_vs_a_vect)),
-                                  rep(paste('Within ', group_b, sep=''), length(b_vs_b_vect)),
-                                  rep('Between', length(between_group_distances))))
-      p = ggplot(df, aes(x=Distance, color=Condition)) + geom_density(alpha=.3) +
-          theme(plot.background = element_rect(fill = "transparent", color = NA))  # Transparent plot background. 
-    }
-    
-    if (! is.null(xlimits)) {
-      p + xlim(xlimits[1], xlimits[2])
-    }
-    if (titles) {
-      p + ylab('Probability density') +
-            xlab('Distance') +
-            ggtitle(paste('Groups: ', group_a, ' and ', group_b,
-                          # Include stats on title.
-                          '; D=', round(ks$statistic, digits=2), ', p=', round(p_value, digits=2), sep=''))
-      ggsave(paste(plotsave_prefix, 'group_distances_', group_a, '_vs_', group_b, '.pdf', sep=''))
-    } else {
-      # This completely removes ALL space and items around the polot itself. 
-      p + theme(legend.position="none",
-                axis.title = element_blank(),  # Turn off axis titles.
-                axis.text = element_blank(),  # Turn off tick label titles.
-                axis.ticks = element_blank(),  # Turn off black tick marks.
-                text = element_blank(),
-                panel.border = element_blank(),
-                plot.margin=grid::unit(c(0,0,0,0),"cm"),
-                axis.ticks.length = unit(0, "pt")
-                )
-      ggsave(paste(plotsave_prefix, 'group_distances_', group_a, '_vs_', group_b, '.pdf', sep=''),
-             width=plot_size[1], height=plot_size[2], units='mm')
-    }
-
-    
     # Write to dataframe of results
     group_differences_ks[group_b, group_a] = ks$statistic
     group_differences_p[group_b, group_a] = p_value
     # Put in the upper triangle too. It's redundant data, but easier for look up. 
     group_differences_ks[group_a, group_b] = group_differences_ks[group_b, group_a]
     group_differences_p[group_a, group_b] = group_differences_p[group_b, group_a]
+    
+    if (graph_distributions) {
+      if (separate_within_distributions == FALSE) {
+        # ggplot needs data in dataframe format
+        df = data.frame(Distance=c(within_group_distances, between_group_distances), 
+                        Condition=c(rep('Within', length(within_group_distances)),
+                                    rep('Between', length(between_group_distances))))
+        p = ggplot(df, aes(x=Distance, fill=Condition)) + 
+            # There are other options; this shows subtble differences in distribution.  
+            # http://www.cookbook-r.com/Graphs/Plotting_distributions_(ggplot2)/
+            geom_density(alpha=.3) +
+            theme(plot.background = element_rect(fill = "transparent", color = NA)) + # Transparent plot background. 
+            coord_cartesian(xlim = xlimits)
+      } else {
+        # ggplot needs data in dataframe format
+        df = data.frame(Distance=c(within_group_distances, between_group_distances), 
+                        Condition=c(rep(paste('Within ', group_a, sep=''), length(a_vs_a_vect)),
+                                    rep(paste('Within ', group_b, sep=''), length(b_vs_b_vect)),
+                                    rep('Between', length(between_group_distances))))
+        p = ggplot(df, aes(x=Distance, color=Condition)) + geom_density(alpha=.3) +
+            theme(plot.background = element_rect(fill = "transparent", color = NA))  # Transparent plot background. 
+      }
+      
+      if (! is.null(xlimits)) {
+        p + xlim(xlimits[1], xlimits[2])
+      }
+      if (titles) {
+        p + ylab('Probability density') +
+              xlab('Distance') +
+              ggtitle(paste('Groups: ', group_a, ' and ', group_b,
+                            # Include stats on title.
+                            '; D=', round(ks$statistic, digits=2), ', p=', round(p_value, digits=2), sep=''))
+        ggsave(paste(plotsave_prefix, 'group_distances_', group_a, '_vs_', group_b, '.pdf', sep=''))
+      } else {
+        # This completely removes ALL space and items around the polot itself. 
+        p + theme(legend.position="none",
+                  axis.title = element_blank(),  # Turn off axis titles.
+                  axis.text = element_blank(),  # Turn off tick label titles.
+                  axis.ticks = element_blank(),  # Turn off black tick marks.
+                  text = element_blank(),
+                  panel.border = element_blank(),
+                  plot.margin=grid::unit(c(0,0,0,0),"cm"),
+                  axis.ticks.length = unit(0, "pt")
+                  )
+        ggsave(paste(plotsave_prefix, 'group_distances_', group_a, '_vs_', group_b, '.pdf', sep=''),
+               width=plot_size[1], height=plot_size[2], units='mm')
+      }
+    }
+  }
+  if (! is.na(datasave_prefix)) {
+    dir.create(datasave_prefix, showWarnings = FALSE)  # Create if it doesn't already exist. 
+    write.csv(group_differences_ks, paste0(datasave_prefix, '/group_differences_KS.csv'), quote = FALSE)
+    write.csv(group_differences_p, paste0(datasave_prefix, '/group_differences_pvalue.csv'), quote = FALSE)
   }
   return(list(group_differences_ks=group_differences_ks, 
               group_differences_p=group_differences_p))
